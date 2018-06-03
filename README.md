c49387a1d3f0cc0b4c11aba59aef2794d61556f7
Vger <vcdelariva@gmail.com>
2018-06-03 17:53:49 -0500

    Move to separate repository

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Word-Guess-Game
+# Word Guess Game
+
+Word Guess Game is a simple version of a hangman style game. The purpose of this assignment was to create a word guess game using Javascript.
+
+**How to Play:**
+
+Press any Alpha key (A-Z, a-z) to start game. Non alpha keys are ignored. The theme of the game is 'World Cities'. The game will randomly select a world city form its internal list. It will then display a blank word using underscores to represent the letters in the word. It is possible to have a multi-word city such as Los Angeles.
+
+If a correct letter is guessed, the game will reveal the letter in its correct location. If an incorrect letter is selected, then it will add the letter to a list on incorrectly guessed letters and decrement the remaining guesses count.
+
+If the user guesses the correct city name before remaining guesses equal zero, then the game increments the number of wins and restarts the game by selecting a new random city name and reseting the game parameters.
+
+If the user does not guess the correct city name within the allocated number of guesses, then the game resets.
+
+**Assets used:**
+
+Background image from https://www.nasa.gov/feature/goddard/2017/new-night-lights-maps-open-up-possible-real-time-applications
+
+Sounds: https://notificationsounds.com Creative Commons Public License
+
+**Side notes:**
+
+I spent some time trying to get a modal to work when either the user guessed the correct city or ran out of guesses. I was able to get something partially working, but I wasn't happy with its implementation and chose not to include it in the final version. I also didn't spend much time enhancing the website with more CSS. My thought was to enhance its functionality using more Javascript functions (i.e. modals for 'You Win' or 'You Lose'), but went down a rabbit hole and wasn't happy with the result (as already stated above). As I gain more experience, I would like to revisit this exercise and improve both the JS functions (I'm sure there is a more effecient and elegant way to code the game) and the styling (like incorporating the letter buttons that were used in the Fridge class example and other fancy elements.)
---
+++ assets/javascript/game.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+
+    var possibleWords = ["florence", "paris", "madrid", "rome", "singapore", "dubai", "new york city", 
+                            "shanghai", "london", "tokyo", "sydney", "toronto", "beijing", "moscow",
+                            "johannesburg", "istanbul", "warsaw", "jakarta", "kuala lumpur", "mexico city",
+                            "hong kong", "chicago", "seoul", "los angeles", "mumbai"]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
+
+    // Wait for key press
+    document.onkeypress = function(event) {
+        // Make sure key pressed is an alpha character
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    // Game Functions
+    // Check if letter is in word & process
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctSound = document.createElement("audio")
+        var incorrectSound = document.createElement("audio")
+        correctSound.setAttribute("src", "assets/sounds/stairs.mp3")
+        incorrectSound.setAttribute("src","assets/sounds/croak.mp3")
+
+        // Search string for letter
+        for (var i=0, j= wordToMatch.length; i<j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctSound.play()
+                // If guessing word matches random word
+                if (guessingWord.join("") === wordToMatch) {
+                    // Increment # of wins
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame,5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play()
+            // Check if inccorrect guess is already on the list
+            if (!guessedLetters.includes(letter)) {
+                // Add incorrect letter to guessed letter list
+                guessedLetters.push(letter)
+                // Decrement the number of remaining guesses
+                numGuess--
+            }
+            if (numGuess === 0) {
+                // Display word before reseting game
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+
+        updateDisplay()
+
+    }
+    // Check in keypressed is between A-Z or a-z
+    function isAlpha (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+        // Get a new word
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+        // Reset word arrays
+        guessedLetters = []
+        guessingWord = []
+
+        // Reset the guessed word
+        for (var i=0, j=wordToMatch.length; i < j; i++){
+            // Put a space instead of an underscore between multi word "words"
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+        // Update the Display
+        updateDisplay()
+    }
+
+    function updateDisplay () {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ")
+    }
+})
